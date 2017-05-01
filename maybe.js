c0ec69e0d0b0ee99caa7bfbab1ad3f5c7a2aedc1
Marius Jurgelenas <mariusj@wix.com>
2017-05-01 18:59:55 +0300

    maybe implements alt plus alternative

--- maybe.js
+++ maybe.js
@@ -1,14 +1,11 @@
 const daggy = require('daggy')
 const {equals} = require('sanctuary-type-classes')
 
-const Maybe = daggy.taggedSum('Maybe', {Just: ['val'], Nothing: []})
-
-Maybe.fromNullable = function(val) {
-  return val ? Maybe.Just(val) : Maybe.Nothing
-}
-const {Just, Nothing} = Maybe
+const Maybe = daggy.taggedSum('Maybe', {Nothing: [], Just: ['val']})
+const {Nothing, Just} = Maybe
 
 /* Functor */
+// map :: Functor f => f a ~> (a -> b) -> f b
 Maybe.prototype.map = function(fn) {
   return this.cata({
     Nothing: () => Nothing,
@@ -17,6 +14,7 @@ Maybe.prototype.map = function(fn) {
 }
 
 /* Setoid */
+// equals :: Setoid a => a ~> a -> Boolean
 Maybe.prototype.equals = function(setoid) {
   return this.cata({
     Nothing: () => Nothing.is(setoid),
@@ -25,18 +23,25 @@ Maybe.prototype.equals = function(setoid) {
 }
 Maybe.prototype[`fantasy-land/equals`] = Maybe.prototype.equals
 
+
 /* Apply */
+// ap :: Apply f => f a ~> f (a -> b) -> f b
 Maybe.prototype.ap = function(apply) {
   return this.cata({
     Nothing: () => Nothing,
-    Just: val => this.map(apply.val)
+    Just: val => Just.is(apply)
+      ? this.map(apply.val) : Nothing
   })
 }
 
-/* Applicatove */
-Maybe.prototype.of = Just
 
-/* Monad */
+/* Applicatove */
+// of :: Applicative f => a -> f a
+Maybe.of = Just
+
+
+/* Chain */
+// chain :: Chain m => m a ~> (a -> m b) -> m b
 Maybe.prototype.chain = function(fn) {
   return this.cata({
     Nothing: () => Nothing,
@@ -44,7 +49,11 @@ Maybe.prototype.chain = function(fn) {
   })
 }
 
+/* Monad */
+// Maybe is monad because it implements both Chain and Apply
+
 /* Semigroup */
+// concat :: Semigroup a => a ~> a -> a
 Maybe.prototype.concat = function(semigroup) {
   return this.cata({
     Nothing: () => semigroup,
@@ -53,8 +62,34 @@ Maybe.prototype.concat = function(semigroup) {
 }
 
 /* Monoid */
+// empty :: Monoid m => () -> m
 Maybe.empty = function() {
   return Maybe.Nothing
 } 
 
-module.exports = Maybe
+/* Alt */
+//alt :: Alt f => f a ~> f a -> f a
+Maybe.prototype.alt = function(that) {
+  return this.cata({
+    Nothing: () => that,
+    Just: val => Just(val)
+  })
+}
+
+/* Plus */
+//zero :: Plus f => () -> f a
+Maybe.zero = function() {
+  return Nothing
+}
+
+/* Alternative */
+// Maybe is alternative because it implements both Plus and Applicative
+
+/* Outside specification */
+maybeFromNullable = function(val) {
+  return val ? Maybe.Just(val) : Maybe.Nothing
+}
+
+const maybeFork = (val, f, g) => {
+  return Just.is(val) ? val.chain(g) : f()
+}
